0e5d11df30ef27b9a1b48087c052372896caa1db
Ashif <mohamedashif18se@gmail.com>
2024-05-25 01:19:50 +0530

    added instagram link to it

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,45 +1,56 @@
-import './footer.css'
-import { Link } from 'react-router-dom'
+import "./footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <div className="footerContainer">
-        <div className="footerWrapper">
-            <ul className="footerShareList">
-                  <li><a href="#">
-                      <i className="fa-brands fa-facebook"></i>
-                  </a></li>
-                  <li><a href="#">
-                  <i className="fa-brands fa-twitter"></i>
-                  </a></li>
-                  <li><a href="#">
-                      <i className="fa-brands fa-instagram"></i>
-                  </a></li>
-                  <li><a href="#">
-                      <i className="fa-brands fa-linkedin"></i>
-                  </a></li>
-            </ul>
-            <ul className="footerList">
-            <Link to='/'>
-                        <li>Home</li>
-                    </Link>
-                    <Link to='/aboutUs'>
-                        <li>About Us</li>
-                    </Link>
-                    <Link to='/ourServices'>
-                        <li>Our Services</li>
-                    </Link>
-                    <Link to='/Contact'>
-                        <li>Contact</li>
-                    </Link>
-                    <Link to='/JoinTeam'>
-                        <li>Join Our Team</li>
-                    </Link>
-            </ul>
-            <h3><i className="fa-regular fa-copyright"></i><b>2024 WEB DEV GLOBAL INNOVATION </b>| All Rights Reserved.</h3>
-        </div>
+      <div className="footerWrapper">
+        <ul className="footerShareList">
+          <li>
+            <a href="#">
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/webgi21.5/" target="_blank">
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </li>
+        </ul>
+        <ul className="footerList">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/aboutUs">
+            <li>About Us</li>
+          </Link>
+          <Link to="/ourServices">
+            <li>Our Services</li>
+          </Link>
+          <Link to="/Contact">
+            <li>Contact</li>
+          </Link>
+          <Link to="/JoinTeam">
+            <li>Join Our Team</li>
+          </Link>
+        </ul>
+        <h3>
+          <i className="fa-regular fa-copyright"></i>
+          <b>2024 WEB DEV GLOBAL INNOVATION </b>| All Rights Reserved.
+        </h3>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
