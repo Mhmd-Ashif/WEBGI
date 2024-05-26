fd6d42849d811fc1f2445462e41e43b90cf5ed62
Ashif <mohamedashif18se@gmail.com>
2024-05-26 19:29:55 +0530

    about us ✅  need to  make  more attractive  (add patterns)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,12 @@
-import Footer from '../../components/footer/Footer'
-import Navbar from '../../components/navbar/Navbar'
-import './home.css'
-import headerImage from '../../assets/header.jpg'
-import d1 from '../../assets/d1.jpeg'
-import d2 from '../../assets/d2.png'
-import d3 from '../../assets/d3.jpg'
-import d4 from '../../assets/d4.jpg'
-import d5 from '../../assets/d5.jpg'
-
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar/Navbar";
+import "./home.css";
+import headerImage from "../../assets/header.jpg";
+import d1 from "../../assets/d1.jpeg";
+import d2 from "../../assets/d2.png";
+import d3 from "../../assets/d3.jpg";
+import d4 from "../../assets/d4.jpg";
+import d5 from "../../assets/d5.jpg";
 
 function Home() {
   return (
@@ -21,9 +20,7 @@ function Home() {
         </div>
         <div className="homeHeader">
           <div className="homeHeaderLeft">
-            <h1>
-            Unlock the Power of Tamil Nadu's Finest Web Experts
-            </h1>
+            <h1>Unlock the Power of Tamil Nadu's Finest Web Experts</h1>
             <h3>Start Your Journey With Us.</h3>
             <h4>OUR SERVICES</h4>
           </div>
@@ -38,26 +35,26 @@ function Home() {
         <div className="homeSubBody">
           <h1>WHAT WE CAN DO FOR YOU?</h1>
           <div className="cards">
-          <div className="card">
-            <img src={d1} alt="" />
-            <h2>Consultation and Discovery</h2>
-          </div>
-          <div className="card">
-          <img src={d2} alt="" />
-            <h2>Custom Web Design and Development</h2>
-          </div>
-          <div className="card">
-          <img src={d3} alt="" />
-            <h2>Digital Marketing Strategy</h2>
-          </div>
-          <div className="card">
-          <img src={d4} alt="" />
-            <h2>Ongoing Support and Maintenance</h2>
-          </div>
-          <div className="card">
-          <img src={d5} alt="" />
-            <h2>Analytics and Optimization</h2>
-          </div>
+            <div className="card">
+              <img src={d1} alt="" />
+              <h2>Consultation and Discovery</h2>
+            </div>
+            <div className="card">
+              <img src={d2} alt="" />
+              <h2>Custom Web Design and Development</h2>
+            </div>
+            <div className="card">
+              <img src={d3} alt="" />
+              <h2>Digital Marketing Strategy</h2>
+            </div>
+            <div className="card">
+              <img src={d4} alt="" />
+              <h2>Ongoing Support and Maintenance</h2>
+            </div>
+            <div className="card">
+              <img src={d5} alt="" />
+              <h2>Analytics and Optimization</h2>
+            </div>
           </div>
         </div>
         <div className="homeFooter">
@@ -65,7 +62,7 @@ function Home() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
